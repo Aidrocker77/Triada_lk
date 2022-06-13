b7c7a6fe9d2d34685f2829b00de9133f5f107b9a
Айдар Максимов <aidrocker@icloud.com>
2022-06-13 13:17:58 +0300

    chore(): added my projects

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import configureStore from './store/index.js.js';
+import configureStore from './store/index.js';
 import './styles/index.scss';
 import App from './views/App';
 ReactDOM.render(
