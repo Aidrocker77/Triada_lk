e650ffa46fbfbfa8f06d75de0d845256bb9c272b
Alex Smit01~ <AlexSmit17264@gmail.com>
2022-06-13 18:18:52 +0300

    add authorization page

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,12 @@ import { Provider } from 'react-redux'
 import configureStore from './store/index.js';
 import './styles/index.scss';
 import App from './views/App';
+import MyProjects from './views/MyProjects';
+import Authorization from './views/Authorization';
+
 ReactDOM.render(
  <Provider store={configureStore()}>
-  <App />
+  <Authorization />
  </Provider>,
  document.getElementById('root')
 );
